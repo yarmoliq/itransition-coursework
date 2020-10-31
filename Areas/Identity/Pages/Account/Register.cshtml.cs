ccefde062e1f3cc2685b97ebb05a295d88b9947e
Maxim Yarmolik <yarmoliq@gmail.com>
2020-10-31 17:09:50 +0300

    Add namespace

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -61,7 +61,8 @@ namespace coursework_itransition.Areas.Identity.Pages.Account
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
-            [DataType(DataType.Password)]
+            [DataType(DataType.
+            Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
 
