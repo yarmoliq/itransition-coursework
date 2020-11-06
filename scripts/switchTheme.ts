1bb0ed7ffc64ec512c6695bc16567fac98f7eb77
Maxim Yarmolik <yarmoliq@gmail.com>
2020-11-06 19:17:08 +0300

    Theme switch using razor

--- scripts/switchTheme.ts
+++ scripts/switchTheme.ts
@@ -1,6 +1,3 @@
-const bootstrapDark : string = location.href.replace(location.pathname, "/") + "lib/bootstrap/dist/css/bootstrap-darkly.css";
-const bootstrapLight: string = location.href.replace(location.pathname, "/") + "lib/bootstrap/dist/css/bootstrap-flatly.css";
-
 function setCookie(name, value, days = 0) {
     var expires = "";
     if (days) {
@@ -21,28 +18,6 @@ function getCookie(name) {
     }
     return null;
 }
-
-let link = document.createElement('link');
-
-// var file = location.pathname.split("/").pop();
-
-// var link = document.createElement("link");
-// link.href = file.substr(0, file.lastIndexOf(".")) + ".css";
-// link.type = "text/css";
-// link.rel = "stylesheet";
-// link.media = "screen,print";
-
-link.rel = 'stylesheet';
-link.type = 'text/css';
-if (getCookie("theme") == "dark") {
-    link.href = bootstrapDark;
-}
-else {
-    link.href = bootstrapLight;
-}
-link.media = 'all';
-
-document.getElementsByTagName("head")[0].appendChild(link);
 
 let themeSwitcher = document.getElementById("switch-theme");
 if (themeSwitcher) {
