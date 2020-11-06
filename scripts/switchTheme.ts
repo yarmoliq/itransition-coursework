22352afee93576d097d12e5ed8f22990f5443b72
Maxim Yarmolik <yarmoliq@gmail.com>
2020-11-06 19:26:12 +0300

    Extract get/set cookie into separate file

--- scripts/switchTheme.ts
+++ scripts/switchTheme.ts
@@ -1,32 +1,13 @@
-function setCookie(name, value, days = 0) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
+import * as cookies from "./cookies.js"
 
 let themeSwitcher = document.getElementById("switch-theme");
 if (themeSwitcher) {
     themeSwitcher.addEventListener("click", () => {
-        if (getCookie("theme") == "dark") {
-            setCookie("theme", "light");
+        if (cookies.getCookie("theme") == "dark") {
+            cookies.setCookie("theme", "light");
         }
         else {
-            setCookie("theme", "dark");
+            cookies.setCookie("theme", "dark");
         }
         location.reload();
     });
