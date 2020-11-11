98a373dcd653a6bdf0b7e062823a80cc8114a8ae
Maxim Yarmolik <yarmoliq@gmail.com>
2020-11-11 18:22:41 +0300

    add tabs to edit page

---
+++ scripts/Chapter/Edit.ts
@@ -0,0 +1,15 @@
+let tabMD: HTMLDivElement                       = <HTMLDivElement>document.getElementById("tab-md");
+const inputTitle: HTMLInputElement              = <HTMLInputElement>document.getElementById("input-title");
+const textareaContents: HTMLTextAreaElement     = <HTMLTextAreaElement>document.getElementById("textarea-contents");
+
+let previewTitle: HTMLParagraphElement          = <HTMLParagraphElement>document.getElementById("md-preview-title");
+let previewContents: HTMLDivElement             = <HTMLParagraphElement>document.getElementById("md-preview-contents");
+
+tabMD.addEventListener('click', () => {
+    console.log("HERE");
+    const title     : string = inputTitle.value;
+    const contents  : string = textareaContents.value;
+
+    previewTitle.innerHTML      = title;
+    previewContents.innerHTML   = contents;
+});
