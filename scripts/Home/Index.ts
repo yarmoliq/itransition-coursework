04fa30b9712eb008e682b4e405d67a206af7899b
Bogomaz Dmitry <bogomaz.dima2013@gmail.com>
2020-11-14 10:37:25 +0300

    Add button go-up and infinity page

---
+++ scripts/Home/Index.ts
@@ -0,0 +1,30 @@
+let arrowTop = document.getElementById("arrowTop");
+let pol = document.getElementById("pol");
+
+function addPost() {
+    let div = document.createElement("div");
+    div.innerHTML = "hjcjvhx";
+    pol.appendChild(div);
+}
+
+function populate() {
+    while (true) {
+        let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
+        if (windowRelativeBottom < document.documentElement.clientHeight + 100) {
+            addPost();
+        }
+        else{
+            break;
+        }
+    }
+}
+window.addEventListener("scroll", function(){
+    arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
+    populate();
+});
+
+populate();
+
+arrowTop.addEventListener('click', function(){
+    window.scrollTo(pageXOffset, 0);
+});
